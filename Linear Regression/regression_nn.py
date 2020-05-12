66d5bb5f2d30d9a2e155ac3e9df099fad2618308
NetworkClassificationProject <isurualagiyawanna9717@gmail.com>
2020-05-12 17:02:10 +0530

    # tune hyperparam

--- Linear Regression/regression_nn.py
+++ Linear Regression/regression_nn.py
@@ -30,20 +30,20 @@ class RegressionNN(object):
     def loss_fnc(self, Ypred, Y):
         return F.mse_loss(Ypred, Y)
 
-    def optimizer(self, learning_rate=0.01):
+    def optimizer(self, learning_rate=0.1):
         return Adam(self.regressor().parameters(), lr=learning_rate)
 
-    def train(self, num_epochs=30):
+    def train(self, num_epochs=100):
         model = self.regressor()
         opt = self.optimizer()
-        for _ in range(num_epochs):
+        for i in range(1,num_epochs+1):
             train_loss = 0
             for X, Y in self.train_data:
                 Ypred = model(X)
                 loss = self.loss_fnc(Ypred, Y)
-                loss.backward()
+                loss.backward() # calculate gradients
 
-                opt.step()
+                opt.step() # update parameters using claculated gradients
                 opt.zero_grad() # use to avoid accumilating the gradients
                 train_loss += loss.item()
 
@@ -53,8 +53,8 @@ class RegressionNN(object):
                     Ypred = model(X)
                     loss = self.loss_fnc(Ypred, Y)
                     test_loss += loss.item()
-
-            print("Train loss : {} Test loss : {}".format(train_loss, test_loss))
+            if i % 10 == 0:
+                print("Train loss : {} Test loss : {}".format(train_loss, test_loss))
 
 if __name__ == "__main__":
     train_data, test_data = get_data()
